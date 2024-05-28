584ab3aca0caeb675b21fbe0e9e87b5abeee3f69
ezy <249736246@qq.com>
2024-05-28 18:29:11 +0800

    #

--- systembars/src/main/java/me/reezy/cosmo/systembars/utility.kt
+++ systembars/src/main/java/me/reezy/cosmo/systembars/utility.kt
@@ -2,7 +2,6 @@
 
 package me.reezy.cosmo.systembars
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Dialog
 import android.os.Build
@@ -49,6 +48,7 @@ var Window.isDrawsSystemBarBackgrounds: Boolean
 var Window.isStatusBarOverlay: Boolean
     get() = decorView.systemUiVisibility.hasFlags(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
     set(value) = setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or if (value) View.SYSTEM_UI_FLAG_LAYOUT_STABLE else 0, value)
+
 var Window.isNavigationBarOverlay: Boolean
     get() = decorView.systemUiVisibility.hasFlags(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
     set(value) = setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or if (value) View.SYSTEM_UI_FLAG_LAYOUT_STABLE else 0, value)
@@ -56,9 +56,7 @@ var Window.isNavigationBarOverlay: Boolean
 var Window.isStatusBarLight: Boolean
     get() = when {
         Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> insetsController!!.systemBarsAppearance.hasFlags(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
-        Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> decorView.systemUiVisibility.hasFlags(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
-        RomUtil.isOppo() -> decorView.systemUiVisibility.hasFlags(0x00000010)
-        else -> false
+        else -> decorView.systemUiVisibility.hasFlags(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
     }
     set(value) {
         when {
@@ -66,8 +64,7 @@ var Window.isStatusBarLight: Boolean
                 val flag = WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
                 insetsController?.setSystemBarsAppearance(if (value) flag else 0, flag)
             }
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR, value)
-            else -> { }
+            else -> setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR, value)
         }
     }
 
@@ -84,9 +81,7 @@ var Window.isNavigationBarLight: Boolean
                 insetsController?.setSystemBarsAppearance(if (value) flag else 0, flag)
             }
             Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR, value)
-            RomUtil.isMiui6() -> setDarkModeForMIUI6("EXTRA_FLAG_NAVIGATION_BAR_DARK_MODE", value)
-            else -> {
-            }
+            else -> {}
         }
     }
 
@@ -96,20 +91,7 @@ fun Window.setFlags(flags: Int, enable: Boolean) {
     setFlags(if (enable) flags else 0, flags)
 }
 
+
 fun Window.setSystemUiVisibility(flags: Int, enable: Boolean) {
     decorView.systemUiVisibility = if (enable) decorView.systemUiVisibility or flags else decorView.systemUiVisibility and flags.inv()
-}
-
-@SuppressLint("PrivateApi")
-internal fun Window.setDarkModeForMIUI6(key: String, dark: Boolean) {
-    try {
-        val classLayoutParams = Class.forName("android.view.MiuiWindowManager\$LayoutParams")
-        val fieldDarkMode = classLayoutParams.getField(key)
-        val flag = fieldDarkMode.getInt(classLayoutParams)
-
-        val setExtraFlags = javaClass.getMethod("setExtraFlags", Int::class.javaPrimitiveType, Int::class.javaPrimitiveType)
-        setExtraFlags(this, if (dark) flag else 0, flag)
-    } catch (e: Exception) {
-    }
-
 }
